79a6a1726a3551ab7af909db3f67475c30a0cdec
hans-peter stoerr <hans-peter.stoerr@email.de>
2008-11-15 20:13:01 +0100

    Impl feigalpha komplett aber nonfunc

--- src/test/java/net/stoerr/feigenbaum/approx/ApproximationTest.java
+++ src/test/java/net/stoerr/feigenbaum/approx/ApproximationTest.java
@@ -44,20 +44,31 @@ public class ApproximationTest extends AbstractJScienceTest<FloatingPoint> {
         assertTrue(maxdif < 1e-5);
     }
 
-    /** */
-    private double checkInitialApprox(final F<Double, Double> F) {
+    private double checkInitialApprox(final F<Double, Double> f) {
         double maxdif = 0;
-        final Vector<FloatingPoint> init = a.initialApproximation(h.wrap(F));
+        final Vector<FloatingPoint> init = a.initialApproximation(h.wrap(f));
         System.out.println(init);
         for (int i = 0; i <= 10; ++i) {
             final double dx = i * 0.1;
             FloatingPoint x = h.v(dx);
-            final double fv = F.call(dx);
+            final double fv = f.call(dx);
             final FloatingPoint iv = a.pol.value(init, x);
             double dif = Math.abs(fv - h.d(iv));
             // System.out.println(dx + "\t" + fv + "\t" + dif + "\t" + iv);
             maxdif = Math.max(dif, maxdif);
         }
         return maxdif;
+    }
+    
+    public void testImprovement() {
+        Vector<FloatingPoint> g = a.initialApproximation(h.wrap(FREN));
+        FloatingPoint off = a.evaluateApproximation(g);
+        System.out.println(off);
+        for (int i=0; i<10; ++i) {
+            g = a.improveApproximation(g);
+            off = a.evaluateApproximation(g);
+            System.out.println(off);
+            
+        }
     }
 }
