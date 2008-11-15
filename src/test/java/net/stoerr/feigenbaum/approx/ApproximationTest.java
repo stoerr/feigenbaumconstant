42f2f0ab5452ad18bea8ac9dccabc4f0ea7f38eb
hans-peter stoerr <hans-peter.stoerr@email.de>
2008-11-15 19:06:08 +0100

    .

--- src/test/java/net/stoerr/feigenbaum/approx/ApproximationTest.java
+++ src/test/java/net/stoerr/feigenbaum/approx/ApproximationTest.java
@@ -1,7 +1,5 @@
 package net.stoerr.feigenbaum.approx;
 
-import static org.junit.Assert.*;
-
 import org.jscience.mathematics.number.FloatingPoint;
 import org.jscience.mathematics.vector.Vector;
 import org.junit.Test;
@@ -9,10 +7,15 @@ import org.junit.Test;
 import net.stoerr.feigenbaum.FeigenConstants;
 import net.stoerr.feigenbaum.basic.AbstractJScienceTest;
 import net.stoerr.feigenbaum.basic.NumHelper;
+import net.stoerr.feigenbaum.util.F;
+
+import static net.stoerr.feigenbaum.FeigenConstants.*;
 
 public class ApproximationTest extends AbstractJScienceTest<FloatingPoint> {
 
     private Approximation<FloatingPoint> a;
+
+    private int digitsold;
 
     public ApproximationTest() {
         super(NumHelper.REAL);
@@ -21,18 +24,40 @@ public class ApproximationTest extends AbstractJScienceTest<FloatingPoint> {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        a = new Approximation<FloatingPoint>(9, h);
+        a = new Approximation<FloatingPoint>(7, h);
+        digitsold = FloatingPoint.getDigits();
+        // FloatingPoint.setDigits(30);
     }
 
-    @Test
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        FloatingPoint.setDigits(digitsold);
+    }
+
     public void testInitialApproximation() {
-        FloatingPoint.setDigits(30);
-        final Vector<FloatingPoint> init = a.initialApproximation(h.wrap(FeigenConstants.FREN));
-        System.out
-                .println(init);
-        for (int i=0; i<=10; ++i) {
-            FloatingPoint x = h.v(i*0.1);
-        }
+        double maxdif = checkInitialApprox(FeigenConstants.FREN);
+        System.out.println("Maxdif FREN" + maxdif);
+        assertTrue(maxdif < 1e-7);
+        maxdif = checkInitialApprox(FeigenConstants.FAPPROX);
+        System.out.println("Maxdif FAPPROX" + maxdif);
+        assertTrue(maxdif < 1e-5);
     }
 
+    /** */
+    private double checkInitialApprox(final F<Double, Double> F) {
+        double maxdif = 0;
+        final Vector<FloatingPoint> init = a.initialApproximation(h.wrap(F));
+        System.out.println(init);
+        for (int i = 0; i <= 10; ++i) {
+            final double dx = i * 0.1;
+            FloatingPoint x = h.v(dx);
+            final double fv = F.call(dx);
+            final FloatingPoint iv = a.pol.value(init, x);
+            double dif = Math.abs(fv - h.d(iv));
+            // System.out.println(dx + "\t" + fv + "\t" + dif + "\t" + iv);
+            maxdif = Math.max(dif, maxdif);
+        }
+        return maxdif;
+    }
 }
