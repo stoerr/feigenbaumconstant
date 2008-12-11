4f380f5b3db13d6b99c5f46d02c13c3766b45282
hans-peter stoerr <hans-peter.stoerr@email.de>
2008-12-11 23:07:29 +0100

    ...

---
+++ src/test/java/org/jscience/mathematics/number/test/AbstractNumberTestSuite.java
@@ -0,0 +1,61 @@
+package org.jscience.mathematics.number.test;
+
+import static javolution.context.LogContext.info;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javolution.testing.TestContext;
+import javolution.testing.TestSuite;
+
+import org.jscience.mathematics.number.Number;
+
+import static javolution.testing.TestContext.*;
+
+public abstract class AbstractNumberTestSuite<T extends Number<T>> extends TestSuite {
+
+    public static final double eps = 1e-7;
+
+    final NumberHelper<T> _helper;
+    final Class _numberClass;
+    List<Pair<Double, T>> _testvalues;
+
+    AbstractNumberTestSuite(NumberHelper<T> helper) {
+        _helper = helper;
+        _numberClass = helper.getNumberClass();
+    }
+
+    @Override
+    public void run() {
+        info(getClass().toString());
+        makeTestValues();
+        for (final Pair<Double, T> p : _testvalues) {
+            test(new AbstractNumberTest<T>("Testing doubleValue ", p._x, _helper) {
+                @Override
+                T operation() throws Exception {
+                    return p._y;
+                }
+            });
+        }
+        for (final Pair<Double, T> p : _testvalues) {
+            test(new AbstractNumberTest<T>("Testing toString / valueOf(String) ", p._x, _helper) {
+                @Override
+                T operation() throws Exception {
+                    return _helper.valueOf(p._y.toString());
+                }
+            });
+        }
+    }
+
+    void makeTestValues() {
+        _testvalues = new ArrayList<Pair<Double, T>>();
+        _testvalues.add(Pair.make(0.0, _helper.getZero()));
+        _testvalues.add(Pair.make(1.0, _helper.getOne()));
+        // for (double d : new double[] { 0.0, 1.0, 0.1, 0.9, -0.1, -0.9, 1.1, 1234, -9876 }) {
+        for (double d : new double[] { 0.0, 1.0, 0.09, 0.9, 1.1, 1234 }) {
+            info("Adding Testvalue " + d);
+            _testvalues.add(Pair.make(d, _helper.valueOf(d)));
+        }
+    }
+
+}
--- src/test/java/org/jscience/mathematics/number/test/FloatingPointTestSuite.java
+++ src/test/java/org/jscience/mathematics/number/test/FloatingPointTestSuite.java
@@ -5,26 +5,34 @@ import java.util.Arrays;
 
 import org.jscience.mathematics.number.FloatingPoint;
 
+import javolution.context.LogContext;
 import javolution.testing.TestCase;
 import javolution.testing.TestContext;
 import javolution.testing.TestSuite;
 
 import static javolution.testing.TestContext.*;
 
-public class FloatingPointTestSuite extends TestSuite {
-    
-    protected final NumberHelper<FloatingPoint> helper = NumberHelper.FLOATINGPOINT;
+public class FloatingPointTestSuite extends AbstractNumberTestSuite<FloatingPoint> {
+
+    public FloatingPointTestSuite() {
+        super(NumberHelper.FLOATINGPOINT);
+    }
 
     @Override
     public void run() {
-        final Method[] m = FloatingPoint.class.getDeclaredMethods();
-        System.out.println(Arrays.asList(m));
-        TestContext.info("Test Suite for LargeInteger");
-        TestContext.test(zeroTest);
+        info("Test Suite for FloatingPoint");
+        super.run();
+        test(zeroTest);
     }
-    
+
     public static void main(String[] args) {
-        new FloatingPointTestSuite().run();
+        LogContext.enter(LogContext.CONSOLE);
+        enter(REGRESSION);
+        try {
+            new FloatingPointTestSuite().run();
+        } finally {
+            exit();
+        }
     }
 
     private TestCase zeroTest = new TestCase() {
@@ -32,15 +40,15 @@ public class FloatingPointTestSuite extends TestSuite {
         @Override
         public void execute() {
             try {
-                FloatingPoint val = helper.getOne();
+                FloatingPoint val = _helper.getOne();
                 System.out.println(val);
-                val = helper.valueOf(0.12);
+                val = _helper.valueOf(0.12);
                 System.out.println(val);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
-        
+
     };
 
 }
