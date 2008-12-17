4561115d69d0071186355b03c1cfbf7770c0f1e8
Hans-Peter Stoerr <Hans-Peter.Stoerr@t-systems-mms.de>
2008-12-17 11:46:24 +0100

    more tests

--- src/test/java/org/jscience/mathematics/number/test/RealTestSuite.java
+++ src/test/java/org/jscience/mathematics/number/test/RealTestSuite.java
@@ -1,5 +1,10 @@
 package org.jscience.mathematics.number.test;
 
+import static javolution.context.LogContext.info;
+import static javolution.testing.TestContext.assertEquals;
+import static javolution.testing.TestContext.test;
+import javolution.testing.TestCase;
+
 import org.jscience.mathematics.number.Real;
 
 public class RealTestSuite extends AbstractNumberTestSuite<Real> {
@@ -7,5 +12,30 @@ public class RealTestSuite extends AbstractNumberTestSuite<Real> {
     public RealTestSuite() {
         super(NumberHelper.REAL);
     }
-    
+
+    /**
+     * The implementation of Real.equals differs from the usual implementation since Real are not only a value but an
+     * error as well. This means that Real.ONE.equals(Real.valueOf(1.0)) is false, since Real.valueOf(1.0) has an error
+     * of 23.
+     */
+    @Override
+    protected void testEquals() {
+        info("  equals");
+        for (final Pair<Double, Real> p : getTestValues()) {
+            for (final Pair<Double, Real> q : getTestValues()) {
+                test(new TestCase() {
+                    @Override
+                    public void execute() {
+                        try {
+                            assertEquals(p + "," + q, p == q || (p._x == 0 && q._x == 0), p._y.equals(q._y));
+                        } catch (Throwable t) {
+                            boolean cmp = p._y.equals(q._y);
+                            assertEquals(p + "," + q, p == q, cmp);
+                        }
+                    }
+                });
+            }
+        }
+    }
+
 }
