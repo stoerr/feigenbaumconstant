041ffa59cdc6bbe4ed3e6330a01183ec6067ad56
hans-peter <hans-peter@stoerr.net>
2008-12-23 11:44:39 +0100

    Start to clean things up.

--- src/test/java/org/jscience/mathematics/number/test/LargeIntegerTestSuite.java
+++ src/test/java/org/jscience/mathematics/number/test/LargeIntegerTestSuite.java
@@ -1,3 +1,11 @@
+/*
+ * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
+ * Copyright (C) 2007 - JScience (http://jscience.org/)
+ * All rights reserved.
+ * 
+ * Permission to use, copy, modify, and distribute this software is
+ * freely granted, provided that this notice is preserved.
+ */
 package org.jscience.mathematics.number.test;
 
 import java.util.List;
