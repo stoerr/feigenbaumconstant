4638d4ac1e90bae72861c75accb34c322c09bcfe
hans-peter stoerr <hans-peter.stoerr@email.de>
2009-02-02 08:15:36 +0100

    add other testsuites

---
+++ src/test/java/org/jscience/mathematics/number/SimpleComplexTestSuite.java
@@ -0,0 +1,34 @@
+package org.jscience.mathematics.number;
+
+/**
+ * A simple testsuite for complex that tests the operations for {@link Complex} numbers with zero real value by
+ * leveraging {@link AbstractFloatTestSuite}. More tests are needed.
+ * @author hps
+ * @since 02.02.2009
+ */
+public class SimpleComplexTestSuite extends AbstractFloatTestSuite<Complex> {
+
+    SimpleComplexTestSuite() {
+        super(NumberHelper.COMPLEX);
+    }
+
+    @Override
+    protected void testAbs() {
+        // Complex has no abs, which is a bug in itself. 8-)
+    }
+
+    @Override
+    protected void testIsNegative() {
+        // not implemented.
+    }
+
+    @Override
+    protected void testIsPositive() {
+        // not implemented.
+    }
+
+    @Override
+    protected void testIsZero() {
+        // not implemented.
+    }
+}
